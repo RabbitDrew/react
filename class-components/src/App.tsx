6f45b9889d29446c3af931e6b5704f39488400e2
Andrew <andrei.sp.93@gmail.com>
2025-07-13 23:09:40 +0300

    feat: add basic layout and styles

---
+++ class-components/src/App.tsx
@@ -0,0 +1,12 @@
+import { Header } from './core/components/header/header';
+import { ResultList } from './core/components/result-list/result-list';
+function App() {
+  return (
+    <>
+    <Header />
+    <ResultList/>
+    </>
+  );
+}
+
+export default App;
