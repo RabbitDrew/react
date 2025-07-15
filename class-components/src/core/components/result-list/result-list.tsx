faf7a460d9159893fea58b0499d6a2f8c9880ff0
Andrew Sparish <Andrei.sp.93@gmail.com>
2025-07-15 12:39:19 +0300

    feat: add style to item list and err-boundery btn

--- class-components/src/core/components/result-list/result-list.tsx
+++ class-components/src/core/components/result-list/result-list.tsx
@@ -4,27 +4,54 @@ export const ResultList = () => {
     <>
       <main className="result-list__wrapper">
         <section className="result-list__header">
-          <h2 className="result-list-header-title">item</h2>
-          <h2 className="result-list-header-title">result</h2>
+          <h2 className="result-list-header-title">Item</h2>
+          <h2 className="result-list-header-title">Result</h2>
         </section>
 
         <section className="rersult-list__body">
-          <ul className="result-list__item">
+          <ul className="result-list">
             <li className="result-list-item">
-              <h2 className="result-list-title">item-title</h2>
+                <h2 className="result-list-title">item-title</h2>
+               
+                <h2 className="result-list-title">item-result</h2>
             </li>
-          </ul>
 
-          <ul className="result-list__result">
-            <li className="result-list-item">
-              <h2 className="result-list-title">item-result</h2>
+             <li className="result-list-item">
+                <h2 className="result-list-title">item-title</h2>
+               
+                <h2 className="result-list-title">item-result</h2>
+            </li>
+
+             <li className="result-list-item">
+                <h2 className="result-list-title">item-title</h2>
+               
+                <h2 className="result-list-title">item-result</h2>
+            </li>
+
+
+             <li className="result-list-item">
+                <h2 className="result-list-title">item-title</h2>
+               
+                <h2 className="result-list-title">item-result</h2>
+            </li>
+
+             <li className="result-list-item">
+                <h2 className="result-list-title">item-title</h2>
+               
+                <h2 className="result-list-title">item-result</h2>
+            </li>
+
+             <li className="result-list-item">
+                <h2 className="result-list-title">item-title</h2>
+               
+                <h2 className="result-list-title">item-result</h2>
             </li>
           </ul>
         </section>
 
         <section className="err-btn__wrapper">
           <div className="err-btn">
-            <h2 className="err-btn-title">error-boundary</h2>
+            <h2 className="err-btn-title">Error-boundary</h2>
           </div>
         </section>
       </main>
