984ec39c2a9b1b35b6c8d26669db1527ec43c524
Andrew <andrei.sp.93@gmail.com>
2025-07-15 08:36:04 +0300

    feat: change style

--- class-components/src/core/components/header/header.tsx
+++ class-components/src/core/components/header/header.tsx
@@ -4,13 +4,16 @@ export const Header = () => {
   return (
     <>
       <header className="page-header">
-        <div className="header-input__wrapper">
-          <input className="header-input" type="text" placeholder="Search" />
-        </div>
+        <div className="page-header__wrapper">
+          <div className="header-input__wrapper">
+            <input className="header-input" type="text" placeholder="Search" />
+            <div className="header-input__style-line"></div>
+          </div>
 
-        <div className="header-search-btn__wrapper">
-          <div className="search-btn">
-            <h2 className="search-btn-title">Search</h2>
+          <div className="header-search-btn__wrapper">
+            <div className="search-btn">
+              <h2 className="search-btn-title">Search</h2>
+            </div>
           </div>
         </div>
       </header>
