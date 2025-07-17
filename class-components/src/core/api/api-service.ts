5b86868ac46881aa1ddfb3777ce3ed19ec070d5c
Andrew <andrei.sp.93@gmail.com>
2025-07-17 03:49:37 +0300

    feat: rework api servive

--- class-components/src/core/api/api-service.ts
+++ class-components/src/core/api/api-service.ts
@@ -31,7 +31,7 @@ interface IData {
 type OptionalIData = Partial<IData>;
 
 export const get = async () => {
-  const url = 'http://stapi.co/api/v1/rest/character/search?title=0413';
+  const url = 'http://stapi.co/api/v1/rest/book/search?';
   try {
     const response = await fetch(url, { method: 'POST' });
     if (!response.ok) {
@@ -45,41 +45,7 @@ export const get = async () => {
 };
 
 class ApiService {
-  categoryEndpont: string;
-  searchEndpoint: string;
-  endpoints: IEndpoint[];
-  key: string;
-  constructor(
-    _categoryEndpont: string,
-    _searchEndpoint: string,
-    _endpoint: IEndpoint[]
-  ) {
-    this.categoryEndpont = _categoryEndpont;
-    this.searchEndpoint = _searchEndpoint;
-    this.endpoints = _endpoint;
-    this.key = '';
-  }
+  queryEndpoint:string
+  
 
-  private createUrl = async (key: string): Promise<string> => {
-    const endpoint = this.endpoints.find(
-      (obj) => this.categoryEndpont === obj.title
-    );
-    const query = encodeURIComponent(key);
-    const url = `https://stapi.co/api/${endpoint?.version}/rest/${endpoint?.endpoint}/serch?title=`;
-    return url;
-  };
-
-  private requestData = async (url: string) => {
-    try {
-      const responce = await fetch(url, { method: 'POST' });
-      if (!responce.ok) {
-        throw new Error(`responce hasn't been got`);
-      }
-      const dataJson = await responce.json();
-    } catch {
-        
-    }
-  };
-
-  private checkKey = async (callback: () => string) => {};
 }
